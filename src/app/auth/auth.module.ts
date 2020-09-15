c02be73a624e02d4c9965427c27c522884e6b7b2
DAO Hamadou <daohamadou@gmail.com>
2020-09-15 22:13:52 +0000

    Premier commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth.component';
+import {MatRippleModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
+import { AuthRoutes } from './auth.routing';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AuthRoutes),
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    AuthComponent
+  ]
+})
+
+export class AuthModule {}
