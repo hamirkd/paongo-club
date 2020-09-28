39de1cab1353709463fd002d3c26fabfa986872c
DAO Hamadou <daohamadou@gmail.com>
2020-09-28 14:35:56 +0000

    Modification de l'affichage

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,12 +18,11 @@ export class DashboardComponent implements OnInit {
 
       chart.on('draw', function(data) {
         if(data.type === 'line' || data.type === 'area') {
-          console.log(data.type);
           data.element.animate({
             d: {
               begin: 600,
               dur: 700,
-              from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+              from: data.path.clone().scale(10, 0).translate(0, data.chartRect.height()).stringify(),
               to: data.path.clone().stringify(),
               easing: Chartist.Svg.Easing.easeOutQuint
             }
